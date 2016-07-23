339c478d828a5d004832c09da5a26c5979b0c874
Chih Tung Chao <me@abbish.com>
2016-07-23 19:59:03 +0800

    rename Main component

---
+++ postnet/abbish/src/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Main.scss';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="main">
+        abbish is a good boy
+      </div>
+    );
+  }
+}
+
+Main.defaultProps = {
+};
+
+export default Main;
--- postnet/abbish/src/index.js
+++ postnet/abbish/src/index.js
@@ -1,6 +1,6 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './components/Main';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('main'));
