3827c087545ba69625497e7fc50d82ba864b7140
Chih Tung Chao <me@abbish.com>
2016-07-24 23:01:51 +0800

    finish Main component by tdd

--- postnet/abbish/src/components/Main.js
+++ postnet/abbish/src/components/Main.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import './Main.scss';
+import { Link } from 'react-router';
 
 class Main extends React.Component {
   render() {
     return (
       <div className="main">
-        abbish is a good boy
+        <div className="main__menu">
+          <Link to="zip-code-to-barcode">Zip Code To Barcode</Link>
+          <Link to="barcode-to-zip-code">Barcode To Zip Code</Link>
+        </div>
+        <div className="main__container">
+          {this.props.children}
+        </div>
       </div>
     );
   }
 }
 
-Main.defaultProps = {
+Main.contextTypes = {
+  router: React.PropTypes.object.isRequired
 };
+
+Main.defaultProps = {};
 
 export default Main;
--- postnet/abbish/src/components/Main.test.js
+++ postnet/abbish/src/components/Main.test.js
@@ -1,5 +1,37 @@
-const assert = require('chai').assert;
+import React from 'react';
+import { assert } from 'chai';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+import { Link } from 'react-router';
+import Main from '../components/Main';
 
 describe('Main component', () => {
 
+  var setup;
+
+  beforeEach(() => {
+    setup = (props = {}, options = {}) => {
+      return shallow(<Main {...props} />, Object.assign({}, {
+        context: {
+          router: {
+            push: sinon.spy()
+          }
+        }
+      }, options));
+    }
+  });
+
+  it('should has link for zip code convert to barcode page', () => {
+    const main = setup();
+    const link = main.find(Link).filterWhere(l => l.prop('to') === 'zip-code-to-barcode');
+
+    assert.equal(link.length, 1);
+  });
+
+  it('should has link for barcode convert to zip code page', () => {
+    const main = setup();
+    const link = main.find(Link).filterWhere(l => l.prop('to') === 'barcode-to-zip-code');
+
+    assert.equal(link.length, 1);
+  });
 });
