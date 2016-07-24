cf8ce2272538c6401d9d7ad3fb3329a0832dc6b2
Chih Tung Chao <me@abbish.com>
2016-07-24 23:30:49 +0800

    finish part of ZipCodeToBarcode by tdd

--- postnet/abbish/src/components/ZipCodeToBarcode.js
+++ postnet/abbish/src/components/ZipCodeToBarcode.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import zipCodeToBarcodeCore from '../libs/zip-code-to-barcode';
 
 class ZipCodeToBarcode extends React.Component {
+
+  constructor() {
+    super();
+
+    this.core = zipCodeToBarcodeCore;
+
+    this.state = {
+      zipCode: null,
+      barcode: null,
+      message: null,
+    };
+
+    this.takeZipCode = this.takeZipCode.bind(this);
+    this.generateBarcode = this.generateBarcode.bind(this);
+  }
+
+  takeZipCode(value) {
+    this.setState({
+      zipCode: value,
+      message: null
+    });
+  }
+
+  generateBarcode() {
+    const barcode = this.core.encodeZipCode(this.state.zipCode);
+
+    this.setState({
+      barcode: barcode,
+      message: barcode ? null : 'Invalid Zip Code'
+    });
+  }
+
   render() {
     return (
       <div className="zip-code-to-barcode">
-        ZipCodeToBarcode
+        <input type="text" placeholder="Zip Code" onChange={this.takeZipCode} />
+        <button onClick={this.generateBarcode}>Generate Barcode</button>
       </div>
     );
   }
--- postnet/abbish/src/components/ZipCodeToBarcode.test.js
+++ postnet/abbish/src/components/ZipCodeToBarcode.test.js
@@ -1,5 +1,134 @@
-const assert = require('chai').assert;
+import React from 'react';
+import { assert } from 'chai';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+import ZipCodeToBarcode from '../components/ZipCodeToBarcode';
 
 describe('ZipCodeToBarcode component', () => {
+
+  var setup;
+
+  beforeEach(() => {
+    setup = (props = {}, options = {}) => {
+      return shallow(<ZipCodeToBarcode {...props} />, Object.assign({}, {
+        context: {
+          router: {
+            push: sinon.spy()
+          }
+        }
+      }, options));
+    }
+  });
+
+  it('should defined zipCode state with default value null', () => {
+    const component = setup();
+
+    assert.equal(component.state('zipCode'), null);
+  });
+
+  it('should defined barcode state with default value null', () => {
+    const component = setup();
+
+    assert.equal(component.state('barcode'), null);
+  });
+
+  it('should defined message state with default value null', () => {
+    const component = setup();
+
+    assert.equal(component.state('message'), null);
+  });
+
+  describe('render', () => {
+    it('should has input for take user input zip code', () => {
+      const component = setup();
+      const input = component.find('input').filterWhere(i => i.prop('placeholder') === 'Zip Code');
+
+      assert.equal(input.length, 1);
+      assert.equal(input.prop('onChange').name, 'takeZipCode');
+    });
+
+    it('should has button for trigger barcode generator', () => {
+      const component = setup();
+      const button = component.find('button').filterWhere(i => i.prop('children') === 'Generate Barcode');
+
+      assert.equal(button.length, 1);
+      assert.equal(button.prop('onClick').name, 'generateBarcode');
+    });
+  });
+
+  describe('takeZipCode', () => {
+    it('should set input value to state zip code when input zip code', () => {
+      const component = setup();
+      const input = component.find('input').filterWhere(i => i.prop('placeholder') === 'Zip Code');
+
+      input.prop('onChange')('12345');
+
+      assert.equal(component.state('zipCode'), '12345');
+    });
+
+    it('should clear state message when input zip code', () => {
+      const component = setup();
+      const input = component.find('input').filterWhere(i => i.prop('placeholder') === 'Zip Code');
+
+      component.setState({
+        message: 'test'
+      });
+
+      assert.equal(component.state('message'), 'test');
+
+      input.prop('onChange')('12345');
+
+      assert.equal(component.state('message'), null);
+    });
+  });
+
+  describe('generateBarcode', () => {
+
+    it('should set generated barcode to state barcode when call encodeZipCode', () => {
+      const component = setup();
+      const instance = component.instance();
+
+      instance.core = {
+        encodeZipCode: sinon.stub().returns('barcode')
+      };
+
+      instance.generateBarcode();
+
+      assert.equal(component.state('barcode'), 'barcode');
+    });
+
+    it('should set error message to state message when call function encodeZipCode and return value is empty', () => {
+      const component = setup();
+      const instance = component.instance();
+
+      instance.core = {
+        encodeZipCode: sinon.stub().returns(null)
+      };
+
+      instance.generateBarcode();
+
+      assert.equal(component.state('barcode'), null);
+      assert.equal(component.state('message'), 'Invalid Zip Code');
+    });
+
+    it('should clear state message when generated valid barcode', () => {
+      const component = setup();
+      const instance = component.instance();
+
+      component.setState({
+        message: 'test'
+      });
+
+      assert.equal(component.state('message'), 'test');
+
+      instance.core = {
+        encodeZipCode: sinon.stub().returns('barcode')
+      };
+
+      instance.generateBarcode();
+
+      assert.equal(component.state('message'), null);
+    });
+  });
 
 });
